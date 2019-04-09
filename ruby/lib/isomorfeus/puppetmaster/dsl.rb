f003e2c7eaf651d9e1ee03c32c75b55e7affb9b5
Jan Biedermann <jan@kursator.de>
2019-04-09 22:27:53 +0200

    add open_new_session and default_document to rspec DSL

--- ruby/lib/isomorfeus/puppetmaster/dsl.rb
+++ ruby/lib/isomorfeus/puppetmaster/dsl.rb
@@ -1,12 +1,19 @@
 module Isomorfeus
   module Puppetmaster
     module DSL
-      def goto(uri)
-        puppetmaster_session.document_goto(puppetmaster_default_document, uri)
-        @puppetmaster_default_document
+      def default_document
+        @puppetmaster_default_document ||= get_default_document
       end
 
+      def goto(uri)
+        puppetmaster_session.document_goto(default_document, uri)
+        @puppetmaster_default_document
+      end
       alias_method :visit, :goto
+
+      def open_new_session(app = nil, driver = nil)
+        Isomorfeus::Puppetmaster.drivers[driver ? driver : Isomorfeus::Puppetmaster.driver].call(app ? app : Isomorfeus::Puppetmaster.served_app)
+      end
 
       def open_new_document(uri = nil)
         puppetmaster_session.document_open_new_document(uri)
@@ -24,17 +31,10 @@ module Isomorfeus
         puppetmaster_session.document_open_new_document('about:blank')
       end
 
-      def puppetmaster_default_document
-        @puppetmaster_default_document ||= get_default_document
-      end
-
       def puppetmaster_session
-        @@puppetmaster_session ||= new_session(Isomorfeus::Puppetmaster.served_app)
+        @@puppetmaster_session ||= open_new_session(Isomorfeus::Puppetmaster.served_app)
       end
 
-      def new_session(app = nil, driver = nil)
-        Isomorfeus::Puppetmaster.drivers[driver ? driver : Isomorfeus::Puppetmaster.driver].call(app ? app : Isomorfeus::Puppetmaster.served_app)
-      end
     end
   end
 end
