1b4013541fe03fa7e145279faa4ff88716571e93
Jan Biedermann <jan@kursator.de>
2020-01-14 16:53:45 +0100

    Update.

--- ruby/spec/drivers/jsdom_spec.rb
+++ ruby/spec/drivers/jsdom_spec.rb
@@ -24,6 +24,7 @@ module DriverSpec
 
     it_behaves_like 'document and node'
     # it_behaves_like '#evaluate_script'
+    # it_behaves_like 'document awaits a promise'
     it_behaves_like '#find'
     it_behaves_like '#find_all'
     it_behaves_like 'html'
